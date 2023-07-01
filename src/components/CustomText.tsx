90ba99be1785af768f979e31faad8a9708eb4b00
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-06-30 21:01:12 -0300

    add explore section

--- src/components/CustomText.tsx
+++ src/components/CustomText.tsx
@@ -9,6 +9,11 @@ interface TypingTextProps {
     textStyles: ReactNode;
 }
 
+interface TitleTextProps {
+    title: ReactNode;
+    textStyles: ReactNode;
+}
+
 
 
 export const TypingText = ({ title, textStyles }: TypingTextProps) => {
@@ -32,6 +37,15 @@ export const TypingText = ({ title, textStyles }: TypingTextProps) => {
     )
 }
 
-export const TitleText = () => {
-    <h2>Title Text</h2>
+export const TitleText = ({ title, textStyles }: TitleTextProps) => {
+    return (
+        <motion.h2
+        variants={textVariant2}
+        initial="hidden"
+        whileInView="show"
+        className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+        >
+            {title}
+        </motion.h2>
+    )
 }
--- src/sections/Explore.tsx
+++ src/sections/Explore.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+'use client';
+
+import { TitleText, TypingText } from '@/components/CustomText';
+import ExploreCard from '@/components/ExploreCard';
+import { exploreWorlds } from '@/constants';
+import styles from '@/styles';
+import { staggerContainer } from '@/utils/motion';
+import { motion } from 'framer-motion'
+import { useState } from 'react';
+
 
 const Explore = () => {
+
+  const [active, setActive] = useState('world-2')
+
+
   return (
-    <div>Explore</div>
+    <section className={`${styles.paddings}`} id='explore'>
+      <motion.div
+        variants={staggerContainer(1, 1)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText title={<>Choose the world you want <br className='md:block hidden' /> to explore</>} textStyles="text-center" />
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {
+            exploreWorlds.map((world, index) => (
+              <ExploreCard
+                index={1}
+                key={world.id}
+                {...world}
+                active={active}
+                handleClick={setActive}
+              />
+            ))
+          }
+        </div>
+      </motion.div>
+    </section>
   )
 }
 
