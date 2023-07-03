c09d91118d09053681704b7fb2b7bd15476827b0
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-07-03 14:57:56 -0300

    add world section

--- src/sections/WhatsNew.tsx
+++ src/sections/WhatsNew.tsx
@@ -16,7 +16,7 @@ const WhatsNew = () => {
         initial='hidden'
         whileInView='show'
         viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gao-8`}
+        className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
       >       
 
         <motion.div
