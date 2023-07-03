5e4c132c45d0344caf9c6d10fb30bc89ac597368
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-07-03 15:26:18 -0300

    add Insight section

--- src/sections/Insights.tsx
+++ src/sections/Insights.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+'use client';
+
+import { TitleText, TypingText } from '@/components/CustomText';
+import InsightCard from '@/components/InsightCard';
+import { insights } from '@/constants';
+import styles from '@/styles';
+import { staggerContainer } from '@/utils/motion';
+import { motion, stagger } from 'framer-motion'
+
+
 
 const Insights = () => {
   return (
-    <div>Insights</div>
+    <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+        variants={staggerContainer(1, 1)}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title='| Insights' textStyles='text-center' />
+        <TitleText title={<>Insight about metaverse</>} textStyles='text-center' />
+
+        <div className="mt-[50px] flex flex-col gap-[30px]">
+          {
+            insights.map((insight, index) => (
+              <InsightCard
+                key={`insight-${index}`}
+                {...insight}
+                index={index + 1}
+              />
+            ))
+          }
+        </div>
+
+
+      </motion.div>
+    </section>
   )
 }
 
